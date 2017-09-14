3451d9f6f83c89cb983f9208b48f37364eef2f54
brburchfield <brburchfield@fullsail.edu>
2017-09-13 22:22:58 -0500

    Mid-candidate Commit

---
+++ MechanicsAssistant/AddBusinessViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddBusinessViewController.swift
+//  MechanicsAssistant
+//
+//  Created by Beau Burchfield on 9/10/17.
+//  Copyright © 2017 Beau Burchfield. All rights reserved.
+//
+
+import Foundation
